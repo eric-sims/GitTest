6a8e1afe90f2030359229914fe379a3db4504bab
Eric Sims <e.sims@icloud.com>
2020-01-31 14:37:49 -0700

    Initial Commit

---
+++ GitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitTest
+//
+//  Created by Eric Sims on 1/31/20.
+//  Copyright © 2020 Eric Sims. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
